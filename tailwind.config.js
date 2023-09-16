522d4105b31178f8bebdad9e2e6afb9a7759b03a
Bludidi <bludidi5@gmail.com>
2023-09-16 19:24:40 +0200

    Add utilitiy classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,15 +38,19 @@ module.exports = {
         price: ['16px', '24px'],
         review: ['12px', '18px'],
         name: ['18px', '21.6px'],
+        mobName: ['16px', '19.2px'],
+        mobReview: ['11px', '16.5px'],
       },
       width: {
         'cardWidth': '355px',
         'starWidth': '10.43px',
+        'mobCardWidth': '158px',
       },
       height: {
         'cardHeight': '467px',
         'imageHeight': '355px',
         'starHeight': '9.99px',
+        'mobCardHeight': '159px',
       },
       borderRadius: {
         'imageRadius': '10px',
