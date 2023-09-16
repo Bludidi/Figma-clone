879a72077fcdf99f6d2760c04675ba9aeed1b50b
Bludidi <bludidi5@gmail.com>
2023-09-16 14:14:10 +0200

    Add more style

--- index.js
+++ index.js
@@ -1,80 +1,84 @@
 const items = [
   {
     name: 'REST IN NATURE T-SHIRT CHARCOAL',
-    reviews: '♥♥♥♥♥ 1234 Reviews',
+    stars: './assets/images/star.png',
+    reviews: '1234 Reviews',
     price: '$104.95',
     image: './assets/images/image_1.jpg',
   },
   {
     name: 'OUTSIDE VIBES T-SHIRT SUNSHINE',
-    reviews: '♥♥♥♥♥ 1234 Reviews',
+    reviews: '1234 Reviews',
     price: '$104.95',
     image: './assets/images/image_2.jpg',
   },
   {
     name: 'HIKE BOTTLE OUTSIDE VIBES FOREST GREEN',
-    reviews: '♥♥♥♥♥ 1234 Reviews',
+    reviews: '1234 Reviews',
     price: '$104.95',
     image: './assets/images/image_3.jpg',
   },
   {
     name: 'OUTSIDE VIBES CAP FOREST GREEN',
-    reviews: '♥♥♥♥♥ 1234 Reviews',
+    reviews: '1234 Reviews',
     price: '$104.95',
     image: './assets/images/image_4.jpg',
   },
   {
     name: 'REST IN NATURE T-SHIRT CHARCOAL',
-    reviews: '♥♥♥♥♥ 1234 Reviews',
+    reviews: '1234 Reviews',
     price: '$104.95',
     image: './assets/images/image_5.jpg',
   },
   {
     name: 'REST IN NATURE T-SHIRT CHARCOAL',
-    reviews: '♥♥♥♥♥ 1234 Reviews',
+    reviews: '1234 Reviews',
     price: '$104.95',
     image: './assets/images/image_6.jpg',
   },
   {
     name: 'OUTSIDE VIBES T-SHIRT SUNSHINE',
-    reviews: '♥♥♥♥♥ 1234 Reviews',
+    reviews: '1234 Reviews',
     price: '$104.95',
     image: './assets/images/image_1.jpg',
   },
   {
     name: 'HIKE BOTTLE OUTSIDE VIBES FOREST GREEN',
-    reviews: '♥♥♥♥♥ 1234 Reviews',
+    reviews: '1234 Reviews',
     price: '$104.95',
     image: './assets/images/image_2.jpg',
   },
   {
     name: 'OUTSIDE VIBES CAP FOREST GREEN',
-    reviews: '♥♥♥♥♥ 1234 Reviews',
+    reviews: '1234 Reviews',
     price: '$104.95',
     image: './assets/images/image_3.jpg',
   },
   {
     name: 'REST IN NATURE T-SHIRT CHARCOAL',
-    reviews: '♥♥♥♥♥ 1234 Reviews',
+    reviews: '1234 Reviews',
     price: '$104.95',
     image: './assets/images/image_4.jpg',
   },
 ];
 
-const itemCards = items.map((item) => `
+const itemCards = items.map(
+  (item) => `
   <div class="w-cardWidth h-cardHeight">
     <ul class="">
       <li><div class="w-cardWidth h-imageHeight"><img src="${item.image}" class="w-cardWidth h-imageHeight rounded-imageRadius" ></div></li>
       <li>${item.name}</li>
-      <li>${item.reviews}</li>
-      <li>${item.price}</li>
+      <li><div class="flex font-poppins font-400 text-grey text-review"><div class="flex my-auto"><img src="${item.stars}" class="w-starWidth h-starHeight"><img src="${item.stars}" class="w-starWidth h-starHeight"><img src="${item.stars}" class="w-starWidth h-starHeight"><img src="${item.stars}" class="w-starWidth h-starHeight"><img src="${item.stars}" class="w-starWidth h-starHeight"></div>&nbsp;<div>${item.reviews}</div></div></li>
+      <li><div class="font-poppins font-500 text-price text-semiBlack">${item.price}</div></li>
     </ul>
   </div>
-`);
+`
+);
 
 const content = document.querySelector('#content');
 const parser = new DOMParser();
 itemCards.forEach((itemCard) => {
-  const itemIndex = parser.parseFromString(itemCard, 'text/html').body.firstChild;
+  const itemIndex = parser.parseFromString(itemCard, 'text/html').body
+    .firstChild;
   content.append(itemIndex);
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,24 +9,45 @@ module.exports = {
       backgroundColor: {
         'dark-green': '#364935',
       },
+      colors: {
+        'default': '#5C553A',
+        'neutral': '#231F20',
+        'semiBlack': '#1C1D1D',
+        'grey': '#707070',
+      },
       padding: {
         'pr': '72px',
         'contentTop': '80px',
         'contentX': '128px',
-        'contentBottom': '100px'
+        'contentBottom': '100px',
       },
       fontFamily: {
         poppins: ['Poppins', 'sans'],
+        serif1: ['Source Serif', 'sans']
+      },
+      fontWeight: {
+        '900': '900',
+        '600': '600',
+        '500': '500',
+        '400': '400',
+      },
+      fontSize: {
+        header2: ['40px', '48px'],
+        btnSm: ['14px', '19.6px'],
+        price: ['16px', '24px'],
+        review: ['12px', '18px']
       },
       width: {
-        'cardWidth': '355px'
+        'cardWidth': '355px',
+        'starWidth': '10.43px',
       },
       height: {
         'cardHeight': '467px',
-        'imageHeight': '355px'
+        'imageHeight': '355px',
+        'starHeight': '9.99px',
       },
       borderRadius: {
-        'imageRadius': '10px'
+        'imageRadius': '10px',
       }
     },
   },
