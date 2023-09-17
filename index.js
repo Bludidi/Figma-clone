4a0a7f63b8245d698adadad0d77d3ed571365707
Bludidi <bludidi5@gmail.com>
2023-09-17 10:44:35 +0200

    Add button margins

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ const itemCards = items.map(
   (item) => `
   <div class="sm:w-cardWidth sm:h-cardHeight w-mobCardWidth">
     <ul class="">
-      <li><div class="w-mobCardWidth h-mobCardHeight sm:w-cardWidth sm:h-imageHeight mb-8"><img src="${item.image}" class="w-mobCardWidth h-mobCardHeight sm:w-cardWidth sm:h-imageHeight rounded-imageRadius" ></div></li>
+      <li><div class="w-mobCardWidth h-mobCardHeight sm:w-cardWidth sm:h-imageHeight sm:mb-4 mb-2"><img src="${item.image}" class="w-mobCardWidth h-mobCardHeight sm:w-cardWidth sm:h-imageHeight rounded-imageRadius" ></div></li>
       <li><div class="w-mobCardWidth sm:w-cardWidth py-mobNameYPad px-mobNameXPad sm:px-nameXPad font-bebas font-400 text-mobName sm:text-name whitespace-normal">${item.name}</div></li>
       <li><div class="flex py-mobNameYPad px-mobNameXPad sm:px-nameXPad font-poppins font-400 text-grey text-mobReview sm:text-review"><div class="flex my-auto"><img src="${item.stars}" class="w-starWidth h-starHeight"><img src="${item.stars}" class="w-starWidth h-starHeight"><img src="${item.stars}" class="w-starWidth h-starHeight"><img src="${item.stars}" class="w-starWidth h-starHeight"><img src="${item.stars}" class="w-starWidth h-starHeight"></div>&nbsp;<div>${item.reviews}</div></div></li>
       <li><div class="py-mobNameYPad px-mobNameXPad sm:px-nameXPad font-poppins font-500 text-price text-semiBlack">${item.price}</div></li>
