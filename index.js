c6a7dacf730460d40cdc007f6587be78345b0c93
Bludidi <bludidi5@gmail.com>
2023-09-17 10:32:18 +0200

    Add more style

--- index.js
+++ index.js
@@ -74,10 +74,10 @@ const items = [
 // Populate item cards with data 
 const itemCards = items.map(
   (item) => `
-  <div class="sm:w-cardWidth sm:h-cardHeight w-mobContWidth h-mobContHeigh">
+  <div class="sm:w-cardWidth sm:h-cardHeight w-mobCardWidth">
     <ul class="">
       <li><div class="w-mobCardWidth h-mobCardHeight sm:w-cardWidth sm:h-imageHeight mb-8"><img src="${item.image}" class="w-mobCardWidth h-mobCardHeight sm:w-cardWidth sm:h-imageHeight rounded-imageRadius" ></div></li>
-      <li><div class="w-mobCardWidth sm:w-cardWidth py-mobNameYPad px-mobNameXPad sm:px-nameXPad font-bebas font-400 text-mobName sm:text-name">${item.name}</div></li>
+      <li><div class="w-mobCardWidth sm:w-cardWidth py-mobNameYPad px-mobNameXPad sm:px-nameXPad font-bebas font-400 text-mobName sm:text-name whitespace-normal">${item.name}</div></li>
       <li><div class="flex py-mobNameYPad px-mobNameXPad sm:px-nameXPad font-poppins font-400 text-grey text-mobReview sm:text-review"><div class="flex my-auto"><img src="${item.stars}" class="w-starWidth h-starHeight"><img src="${item.stars}" class="w-starWidth h-starHeight"><img src="${item.stars}" class="w-starWidth h-starHeight"><img src="${item.stars}" class="w-starWidth h-starHeight"><img src="${item.stars}" class="w-starWidth h-starHeight"></div>&nbsp;<div>${item.reviews}</div></div></li>
       <li><div class="py-mobNameYPad px-mobNameXPad sm:px-nameXPad font-poppins font-500 text-price text-semiBlack">${item.price}</div></li>
     </ul>
